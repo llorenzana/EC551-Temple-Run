15a6f210a14f3c0c5d3444deb6a6d62edfc49bb6
Nick Cao <nickcao@nichi.co>
2023-11-09 14:11:19 -0500

    init devShell

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = import nixpkgs { inherit system; }; in with pkgs; {
+        devShell = mkShell {
+          nativeBuildInputs = [ cmake verilator clang-tools ];
+        };
+      });
+}
