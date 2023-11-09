9e6a158a6dd4050db3c91d267cc546aa07892b60
Nick Cao <nickcao@nichi.co>
2023-11-09 14:03:22 -0500

    fix indent of compositor.sv

--- sources/compositor.sv
+++ sources/compositor.sv
@@ -1,18 +1,18 @@
 module compositor #(parameter DEPTH = 4) (
-   input logic [    DEPTH - 1:0]   R_prev,
-   input logic [    DEPTH - 1:0]   G_prev,
-   input logic [    DEPTH - 1:0]   B_prev,
-   input logic                     A_prev,
+   input logic [DEPTH - 1:0]   R_prev,
+   input logic [DEPTH - 1:0]   G_prev,
+   input logic [DEPTH - 1:0]   B_prev,
+   input logic                 A_prev,
 
-   input logic [    DEPTH - 1:0]   R_curr,
-   input logic [    DEPTH - 1:0]   G_curr,
-   input logic [    DEPTH - 1:0]   B_curr,
-   input logic                     A_curr,
+   input logic [DEPTH - 1:0]   R_curr,
+   input logic [DEPTH - 1:0]   G_curr,
+   input logic [DEPTH - 1:0]   B_curr,
+   input logic                 A_curr,
 
-  output logic [    DEPTH - 1:0]   R_next,
-  output logic [    DEPTH - 1:0]   G_next,
-  output logic [    DEPTH - 1:0]   B_next,
-  output logic                     A_next
+  output logic [DEPTH - 1:0]   R_next,
+  output logic [DEPTH - 1:0]   G_next,
+  output logic [DEPTH - 1:0]   B_next,
+  output logic                 A_next
 );
   always_comb begin
     R_next = A_curr ? R_curr : R_prev;
