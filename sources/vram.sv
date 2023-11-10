2e822ccf454a86bd9cc7dc428c5422efb70790da
Nick Cao <nickcao@nichi.co>
2023-11-09 20:11:48 -0500

    optimize bram for size

--- sources/vram.sv
+++ sources/vram.sv
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
-module vram #(parameter DATA_WIDTH=13, ADDR_WIDTH=15, INIT="init.mem") (
+module vram #(parameter DATA_WIDTH=13, SIZE=19200, ADDR_WIDTH=$clog2(SIZE), INIT="init.mem") (
    input logic                     clk,
    input logic                      en,
    input logic [ADDR_WIDTH - 1:0] addr,
   output logic [DATA_WIDTH - 1:0] data
 );
 
-  reg [DATA_WIDTH - 1:0] ram [2 ** ADDR_WIDTH - 1:0];
+  reg [DATA_WIDTH - 1:0] ram [SIZE - 1:0];
 
   initial begin
     $readmemb(INIT, ram);
