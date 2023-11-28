65abf35ce3414d6c1eb7f1959b1b30fb216ead74
Nick Cao <nickcao@nichi.co>
2023-11-28 14:45:35 -0500

    many rework

--- sources/integration.sv
+++ sources/integration.sv
@@ -64,68 +64,55 @@ module integration(
     .valid(valid)
   );
 
-  logic [3:0] R [4:0], G [4:0], B [4:0];
-  logic                         A [4:0];
+  logic [3:0] R [2:0], G [2:0], B [2:0];
+  logic                         A [2:0];
 
-  compositor compositor_i0(
-    .R_prev(R[0]),
-    .G_prev(G[0]),
-    .B_prev(B[0]),
-    .A_prev(A[0]),
-    .R_curr(R[1]),
-    .G_curr(G[1]),
-    .B_curr(B[1]),
-    .A_curr(A[1]),
-    .R_next(R[2]),
-    .G_next(G[2]),
-    .B_next(B[2]),
-    .A_next(A[2])
-  );
-
-  compositor compositor_i1(
-    .R_prev(R[2]),
-    .G_prev(G[2]),
-    .B_prev(B[2]),
-    .A_prev(A[2]),
-    .R_curr(R[3]),
-    .G_curr(G[3]),
-    .B_curr(B[3]),
-    .A_curr(A[3]),
-    .R_next(R[4]),
-    .G_next(G[4]),
-    .B_next(B[4]),
-    .A_next(A[4])
-  );
-
-  logic [12:0] datab, dataf, datah;
-  logic [14:0] addrb, addrf, addrh;
-  logic validb, validf, validh;
-
-  transformer transformer_i0(
+  layer #(.INIT("background.mem")) background(
+    .clk(CLK100MHZ),
     .hdata(hdata),
     .vdata(vdata),
     .hoffset(0),
     .voffset(0),
-    .addr(addrb),
-    .valid(validb)
+    .R_prev(4'b0),
+    .G_prev(4'b0),
+    .B_prev(4'b0),
+    .A_prev(1'b0),
+    .R_next(R[0]),
+    .G_next(G[0]),
+    .B_next(B[0]),
+    .A_next(A[0])
   );
 
-  transformer transformer_i1(
+  layer #(.INIT("logo.mem")) logo(
+    .clk(CLK100MHZ),
     .hdata(hdata),
     .vdata(vdata),
-    .hoffset(offset),
-    .voffset(0),
-    .addr(addrf),
-    .valid(validf)
+    .hoffset(0),
+    .voffset(offset),
+    .R_prev(R[0]),
+    .G_prev(G[0]),
+    .B_prev(B[0]),
+    .A_prev(A[0]),
+    .R_next(R[1]),
+    .G_next(G[1]),
+    .B_next(B[1]),
+    .A_next(A[1])
   );
 
-  transformer transformer_i2(
+  layer #(.INIT("head.mem")) head(
+    .clk(CLK100MHZ),
     .hdata(hdata),
     .vdata(vdata),
     .hoffset(offsetv),
     .voffset(offseth),
-    .addr(addrh),
-    .valid(validh)
+    .R_prev(R[1]),
+    .G_prev(G[1]),
+    .B_prev(B[1]),
+    .A_prev(A[1]),
+    .R_next(R[2]),
+    .G_next(G[2]),
+    .B_next(B[2]),
+    .A_next(A[2])
   );
 
   layer #(.INIT("coin.mem")) coin(
@@ -140,56 +127,14 @@ module integration(
     // .voffset(-400),
     .hoffset(-200 + coinloc),
     .voffset(-40 - 6 * coinloc),
-    .R_prev(R[4]),
-    .G_prev(G[4]),
-    .B_prev(B[4]),
-    .A_prev(A[4]),
+    .R_prev(R[2]),
+    .G_prev(G[2]),
+    .B_prev(B[2]),
+    .A_prev(A[2]),
     .R_next(VGA_R),
     .G_next(VGA_G),
     .B_next(VGA_B),
     .A_next()
   );
-
-  vram vram_i0(
-    .clk(CLK100MHZ),
-    .addr(addrb),
-    .en(validb),
-    .data(datab)
-  );
-
-  defparam vram_i0.INIT = "background.mem";
-
-  vram vram_i1(
-    .clk(CLK100MHZ),
-    .addr(addrf),
-    .en(validf),
-    .data(dataf)
-  );
-
-  defparam vram_i1.INIT = "logo.mem";
-
-  vram vram_i2(
-    .clk(CLK100MHZ),
-    .addr(addrh),
-    .en(validh),
-    .data(datah)
-  );
-
-  defparam vram_i2.INIT = "head.mem";
-
-  assign R[0] = valid ? datab[12:9] : 4'b0;
-  assign G[0] = valid ? datab[ 8:5] : 4'b0;
-  assign B[0] = valid ? datab[ 4:1] : 4'b0;
-  assign A[0] = valid ? datab[   0] : 1'b0;
-
-  assign R[1] = valid ? dataf[12:9] : 4'b0;
-  assign G[1] = valid ? dataf[ 8:5] : 4'b0;
-  assign B[1] = valid ? dataf[ 4:1] : 4'b0;
-  assign A[1] = valid ? dataf[   0] : 1'b0;
-
-  assign R[3] = valid ? datah[12:9] : 4'b0;
-  assign G[3] = valid ? datah[ 8:5] : 4'b0;
-  assign B[3] = valid ? datah[ 4:1] : 4'b0;
-  assign A[3] = valid ? datah[   0] : 1'b0;
 
 endmodule
