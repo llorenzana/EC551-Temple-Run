af63dff14bcb2b4f71d42012251453704cf8c6b9
Nick Cao <nickcao@nichi.co>
2023-11-30 16:17:57 -0500

    generate tree

--- vga_scripts/img2bin.py
+++ vga_scripts/img2bin.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-with Image.open("../artwork/coin.png") as im:
+with Image.open("../artwork/tree.png") as im:
     im = im.resize((160, 120))
     for pixel in im.getdata():
         r, g, b = pixel[0] >> 4, pixel[1] >> 4, pixel[2] >> 4
