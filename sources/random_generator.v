01813c2c68715fd962866c0b080575fb1d1dc78b
Nick Cao <nickcao@nichi.co>
2023-11-30 12:14:23 -0500

    update random generator source

--- sources/random_generator.v
+++ sources/random_generator.v
@@ -5,28 +5,26 @@
 
 module random_generator(
     input clk,
-    input reset,
+    input rst,
     output reg [19:0] random_number
     );
-    
+   
     reg[19:0] rnd;
     reg[4:0] count;
     wire feedback = rnd[19] ^ rnd[16];
-    
-    always@(posedge clk or posedge reset) begin
-        if (~reset) begin
-            rnd <= 20'b1;
-            count <= 0;
-        end else begin
-            if (count == 20) begin
-                count <= 0;
-                random_number = rnd;
-            end else begin
-                rnd <= {rnd[19:0], feedback};
-                count <= count + 1;
-            end   
-        end
-    
+    initial begin
+        rnd <= 20'b1;
+        count <= 0;
     end
-    
+   
+    always@(posedge clk) begin
+        if (count == 20) begin
+            count <= 0;
+            random_number = rnd;
+        end else begin
+            rnd <= {rnd[19:0], feedback};
+            count <= count + 1;
+        end  
+    end
+   
 endmodule
