af8e966484f56a14efb7818721ddd4d243582f3b
Nick Cao <nickcao@nichi.co>
2023-11-29 20:20:55 -0500

    formalize transformer module

--- sources/transformer.sv
+++ sources/transformer.sv
@@ -1,27 +1,30 @@
 `timescale 1ns / 1ps
 
 module transformer #(
-  parameter IWIDTH =                   2,
-            HWIDTH =                  12,
-             HSIZE =                 640,
-            VWIDTH =                  12,
-             VSIZE =                 480,
-            AWIDTH =                  15
+    parameter HWIDTH,
+              VWIDTH,
+              AWIDTH,
+              IWIDTH,
+              HSIZE,
+              VSIZE
 ) (
-   input logic        [HWIDTH - 1:0]   hdata,
-   input logic        [VWIDTH - 1:0]   vdata,
-   input logic signed [HWIDTH - 1:0] hoffset,
-   input logic signed [VWIDTH - 1:0] voffset,
-  output logic        [AWIDTH - 1:0]    addr,
-  output logic                         valid
+    input  logic [HWIDTH - 1:0] hdata,
+    input  logic [VWIDTH - 1:0] vdata,
+    input  logic [HWIDTH - 1:0] hoffset,
+    input  logic [VWIDTH - 1:0] voffset,
+    output logic [AWIDTH - 1:0] addr,
+    output logic                valid
 );
-  logic [HWIDTH - 1 - IWIDTH:0] haddr;
-  logic [VWIDTH - 1 - IWIDTH:0] vaddr;
-  always_comb begin
-    haddr = {hdata + hoffset}[HWIDTH - 1:IWIDTH];
-    vaddr = {vdata + voffset}[VWIDTH - 1:IWIDTH];
 
-    addr  = vaddr * HSIZE[HWIDTH - 1:IWIDTH] + AWIDTH'(haddr);
-    valid = (vaddr < VSIZE[VWIDTH - 1:IWIDTH]) & (haddr < HSIZE[HWIDTH - 1:IWIDTH]);
+  logic [HWIDTH - 1:0] haddr;
+  logic [VWIDTH - 1:0] vaddr;
+
+  always_comb begin
+    haddr = hdata + hoffset;
+    vaddr = vdata + voffset;
+
+    addr  = AWIDTH'(vaddr[VWIDTH-1:IWIDTH] * HSIZE[HWIDTH-1:IWIDTH]) + AWIDTH'(haddr[HWIDTH-1:IWIDTH]);
+    valid = (vaddr < VSIZE) & (haddr < HSIZE);
   end
+
 endmodule
